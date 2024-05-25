a2cc71ca76ae0eb9b78ffff8495711f003235cc8
Mostafa Taheri <taheri.mo@tiddev.com>
2024-05-25 16:09:53 +0330

    revising front panel

--- src/main/java/FrontPanel.java
+++ src/main/java/FrontPanel.java
@@ -1,14 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class FrontPanel extends JPanel {
     private static FrontPanel instance = null;
 
-    private FrontPanel() {
-
+    private FrontPanel(int x, int y, int width, int height) {
+        this.setBounds(x,y,width,height);
+        Rectangle rectangle = this.getBounds();
+        System.out.println("The x cordinate is: "+rectangle.x + "\nThe y cordinate is: " + rectangle.y + "\nThe height is: " + rectangle.height + "\nThe width is: " + rectangle.width);
+        this.setVisible(true);
+        this.setBackground(Color.CYAN);
+        this.revalidate();
     }
 
-    public static FrontPanel getInstance() {
-        if (instance == null) instance = new FrontPanel();
+    public static FrontPanel getInstance(int x,int y,int width,int height) {
+        if (instance == null) instance = new FrontPanel(x,y,width,height);
         return instance;
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
+    private static Logger logger= LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello There!");
-        JFrame jFrame = new JFrame("Welcome to Best OF US");
+        logger.info("Hello There!\nWelcome to Best OF US");
+        JFrame jFrame = new JFrame("Best OF US v0.01");
         JPanel jPanel = new JPanel();
         JButton jButton = new JButton("New Game");
         jPanel.add(jButton);
@@ -14,11 +18,14 @@ public class Main {
         jFrame.setBounds(0, 0, 800, 600);
         jFrame.revalidate();
         jFrame.setVisible(true);
-        var interim = 1l;
-        System.out.println(interim);
+
         jButton.addActionListener(e -> {
             System.out.println("New Game Pressed!");
-            FrontPanel frontPanel = FrontPanel.getInstance();
+            FrontPanel frontPanel = FrontPanel.getInstance(100,100,800,600);
+            jFrame.add(frontPanel);
+            jFrame.remove(jPanel);
+            frontPanel.setVisible(true);
+            jFrame.revalidate();
         });
     }
 }
